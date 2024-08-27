716339ff4d09a385479040b9f92fe9a577a7d086
emmanuelist <emmanuel.paul75@yahoo.com>
2024-08-27 04:55:25 +0100

    fix: Correct mocked return value for `test_public_repos` method in TestGithubOrgClient
    - Fix the mocked return value in the `test_public_repos` method of `TestGithubOrgClient` to use a list of dictionaries instead of a set. The previous implementation incorrectly used sets, causing a `TypeError: 'set' object is not subscriptable` during the list comprehension in the `public_repos` method.
    
    - This fix ensures that the test correctly simulates the expected response structure, allowing `GithubOrgClient.public_repos` to return the appropriate list of repository names.
    
    - Also add assertions to verify that the `_public_repos_url` property and `get_json` function are called with the correct parameters.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,9 +2,11 @@
 import unittest
 from unittest.mock import PropertyMock, patch
 
-from parameterized import parameterized
+import requests
+from parameterized import parameterized, parameterized_class
 
 from client import GithubOrgClient
+from fixtures import apache2_repos, expected_repos, org_payload, repos_payload
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -36,12 +38,11 @@ class TestGithubOrgClient(unittest.TestCase):
             }
             client = GithubOrgClient("google")
             result = client._public_repos_url
-            self.assertEqual(
-                result, "https://api.github.com/orgs/google/repos")
+            self.assertEqual(result,
+                             "https://api.github.com/orgs/google/repos")
 
-    @patch("client.get_json",
-           return_value=[{"name": "repo1"},
-                         {"name": "repo2"}])
+    @patch("client.get_json", return_value=[{"name": "repo1"},
+                                            {"name": "repo2"}])
     def test_public_repos(self, mock_get_json):
         """
         Test that GithubOrgClient.public_repos returns the correct
@@ -60,3 +61,72 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/google/repos"
             )
+
+    @parameterized.expand(
+        [
+            ({"license": {"key": "my_license"}}, "my_license", True),
+            ({"license": {"key": "other_license"}}, "my_license", False),
+        ]
+    )
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test the GithubOrgClient.has_license return the current boolean value
+        on the license key
+        """
+        client = GithubOrgClient("google")
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
+
+@parameterized_class(
+    [
+        {
+            "org_payload": org_payload,
+            "repos_payload": repos_payload,
+            "expected_repos": expected_repos,
+            "apache2_repos": apache2_repos,
+        }
+    ]
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up class method to mock requests.get to return example payloads
+        found in the fixtures.
+        """
+        cls.get_patcher = patch("requests.get",
+                                side_effect=cls.mocked_requests_get)
+        cls.mock_get = cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Tear down class method to stop the patcher.
+        """
+        cls.get_patcher.stop()
+
+    @staticmethod
+    def mocked_requests_get(url):
+        """
+        Mocked requests.get method to return the correct fixtures for the
+        various values of url.
+        """
+        if url == "https://api.github.com/orgs/google":
+            return MockResponse(org_payload)
+        elif url == "https://api.github.com/orgs/google/repos":
+            return MockResponse(repos_payload)
+        return MockResponse(None, 404)
+
+
+class MockResponse:
+    """
+    Mock response class to simulate requests.Response
+    """
+
+    def __init__(self, json_data, status_code=200):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
